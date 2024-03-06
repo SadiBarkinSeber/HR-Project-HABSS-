e3fc63e4e6cfc080f34c003a431e8d90a43ffddf
shohzodirgashev <qdnl64bzpkrxznywp2m4yriyqrcvwhemvtvbhlddks4unej22atq>
2024-03-06 22:21:47 +0300

    gitignore aded

---
+++ HR_PROJECT.Domain/Entities/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_PROJECT.Domain.Entities
+{
+    public class Employee
+    {
+    }
+}
