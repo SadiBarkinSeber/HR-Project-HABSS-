fc3a4c147c849c8b4fcc20357088da3c25f48763
shohzodirgashev <qdnl64bzpkrxznywp2m4yriyqrcvwhemvtvbhlddks4unej22atq>
2024-03-10 21:24:35 +0300

    azure updates i guess

--- HR_PROJECT.Domain/obj/Debug/net8.0/HR_PROJECT.Domain.AssemblyInfo.cs
+++ HR_PROJECT.Domain/obj/Debug/net8.0/HR_PROJECT.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR_PROJECT.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c27d64d52e8e4cc89454c9c9557d1fc50a5531c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f03ddf269d09082a17ecffddfee2d01e5210d19")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR_PROJECT.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR_PROJECT.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
