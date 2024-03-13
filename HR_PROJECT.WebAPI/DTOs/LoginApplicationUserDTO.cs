c7a0eff0da2c420aa33e201229cad8891f32af37
shohzodirgashev <qdnl64bzpkrxznywp2m4yriyqrcvwhemvtvbhlddks4unej22atq>
2024-03-13 13:37:33 +0300

    Minor login fixes.

---
+++ HR_PROJECT.WebAPI/DTOs/LoginApplicationUserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_PROJECT.Application.Features.CQRS.Commands.ApplicationUserCommands
+{
+    public class LoginApplicationUserDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
