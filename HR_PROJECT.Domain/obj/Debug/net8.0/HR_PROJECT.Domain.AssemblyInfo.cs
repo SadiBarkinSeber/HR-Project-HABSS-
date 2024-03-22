537f40a8905976fa0fae9b45db4fe714d5cefca1
shohzodirgashev <qdnl64bzpkrxznywp2m4yriyqrcvwhemvtvbhlddks4unej22atq>
2024-03-22 16:33:12 +0300

    test

--- HR_PROJECT.Domain/obj/Debug/net8.0/HR_PROJECT.Domain.AssemblyInfo.cs
+++ HR_PROJECT.Domain/obj/Debug/net8.0/HR_PROJECT.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR_PROJECT.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01cb85e6e434b1576f8881e684830349749ec5bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c8ec7d4d5c00d1be782b7dd1c5e42d8abc6c10e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR_PROJECT.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR_PROJECT.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
