0cf78eecf5f7956c4e240963d0524e4c770a2c78
shohzodirgashev <qdnl64bzpkrxznywp2m4yriyqrcvwhemvtvbhlddks4unej22atq>
2024-03-31 21:08:58 +0300

    User creation upon emplyoee creation logic implemented.

--- HR_PROJECT.WebAPI/Controllers/EmployeesController.cs
+++ HR_PROJECT.WebAPI/Controllers/EmployeesController.cs
@@ -2,6 +2,8 @@
 using HR_PROJECT.Application.Features.CQRS.Handlers.EmployeeHandlers.Read;
 using HR_PROJECT.Application.Features.CQRS.Handlers.EmployeeHandlers.Write;
 using HR_PROJECT.Application.Features.CQRS.Queries.EmployeeQueries;
+using HR_PROJECT.WebAPI.DTOs.ApplicationUserDTOs;
+using HR_PROJECT.WebAPI.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -23,14 +25,22 @@ namespace HR_PROJECT.WebAPI.Controllers
 
         #endregion
 
+        #region Services
+
+        private readonly IAuthService _authService;
+
+        #endregion
+
         #region Constructor
-        public EmployeesController(CreateEmployeeCommandHandler employeeCommandHandler, GetEmployeeByIdQueryHandler employeeByIdQueryHandler, GetEmployeeQueryHandler employeeQueryHandler, UpdateEmployeeCommandHandler updateEmployeeCommandHandler, RemoveEmployeeCommandHandler removeEmployeeCommandHandler)
+        public EmployeesController(CreateEmployeeCommandHandler employeeCommandHandler, GetEmployeeByIdQueryHandler employeeByIdQueryHandler, GetEmployeeQueryHandler employeeQueryHandler, UpdateEmployeeCommandHandler updateEmployeeCommandHandler, RemoveEmployeeCommandHandler removeEmployeeCommandHandler, IAuthService authService)
         {
             _createEmployeeCommandHandler = employeeCommandHandler;
             _getEmployeeByIdQueryHandler = employeeByIdQueryHandler;
             _getEmployeeQueryHandler = employeeQueryHandler;
             _updateEmployeeCommandHandler = updateEmployeeCommandHandler;
             _removeEmployeeCommandHandler = removeEmployeeCommandHandler;
+
+            _authService = authService;
         }
         #endregion
 
@@ -60,8 +70,35 @@ namespace HR_PROJECT.WebAPI.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateEmployee(CreateEmployeeCommand command)
         {
-            await _createEmployeeCommandHandler.Handle(command);
-            return Ok("Çalışan bilgileri eklendi.");    
+            CreateApplicationUserDTO dto = new CreateApplicationUserDTO()
+            {
+                Firstname = command.FirstName,
+                Lastname = command.FirstSurname,
+                UserName = command.FirstName.ToLower()
+            };
+
+            try
+            {
+                var (status, response) = await _authService.CreateUser(dto);
+
+                if (!(status == 1))
+                {
+                    return BadRequest(response.Message);
+                }
+
+                command.UserId = response.Id;
+                
+                await _createEmployeeCommandHandler.Handle(command);
+
+                
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+              
         }
 
         //[Authorize(Roles = "manager")]
---
+++ HR_PROJECT.WebAPI/DTOs/ApplicationUserDTOs/CreateUserResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HR_PROJECT.WebAPI.DTOs.ApplicationUserDTOs
+{
+    public class CreateUserResponseDTO
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public string Password { get; set; }
+    }
+}
